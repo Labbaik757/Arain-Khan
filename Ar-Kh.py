20fa7addb384cf7177cd702396632b2a85e8c262
Kashif Ali <technicalkashifmehrvi@gmail.com>
2025-11-08 16:36:22 +0500

    Update current version from 0.6 to 0.7

--- Ar-Kh.py
+++ Ar-Kh.py
@@ -48,7 +48,7 @@ def pro_banner():
 \x1b[1;96m   ➤ \x1b[1;97mCreator        : \x1b[1;96mMAAZ
 \x1b[1;96m   ➤ \x1b[1;97mOperated By    : \x1b[1;92mMAAZ \x1b[1;91m(\x1b[1;90mPremium Access\x1b[1;91m)
 \x1b[1;96m   ➤ \x1b[1;97mTool Access    : \x1b[1;93mPAID 
-\x1b[1;96m   ➤ \x1b[1;97mCurrent Version: \x1b[1;95m0.6
+\x1b[1;96m   ➤ \x1b[1;97mCurrent Version: \x1b[1;95m0.7
 \x1b[1;92m───────────────────────────────────────────────"""
 
 def linex():
